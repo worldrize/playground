efa2aaea6a014f8ac648d0fb80a8aa69f5d7f84f
wakame_tech <kamata1919@gmail.com>
2021-05-17 00:52:26 +0900

    add: playAll(), play next track auto

--- lib/pages/voice_cache_page.dart
+++ lib/pages/voice_cache_page.dart
@@ -1,3 +1,6 @@
+import 'dart:collection';
+
+import 'package:assets_audio_player/assets_audio_player.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
@@ -8,7 +11,7 @@ class CacheableFirebaseStorage {
   /// Storageから取得 or キャッシュを使用
   Future<Uri> getUri(String path) async {
     final url = await FirebaseStorage.instance.ref(path).getDownloadURL();
-    print('storage url: $url');
+    // print('storage url: $url');
     final file = await DefaultCacheManager().getSingleFile(url);
     return file.uri;
   }
@@ -20,59 +23,181 @@ class CacheableFirebaseStorage {
 }
 
 /// 抽象的なプレイヤー
+/// - 再生する音声はキューで管理されている
+/// - キューが残っている場合は自動ですべて再生される
+/// - play(), playAll() は現在のキューを削除し, キューに追加する
 abstract class IPlayer {
-  play(Uri uri);
+  /// キューを取得
+  Queue<Uri> getQueue();
 
-  stop();
+  /// 音声をキューに追加, すでにキューがある場合削除する
+  Future<void> playAll(List<Uri> uris);
+
+  /// 音声を再生, すでにキューがある場合削除する
+  Future<void> play(Uri uri);
+
+  /// 音声を一時停止
+  void pause();
 }
 
 class JustAudioPlayer implements IPlayer {
   AudioPlayer _player = AudioPlayer();
+  Queue<Uri> _playQueue = Queue();
+  Duration _interval = Duration(milliseconds: 300);
 
-  @override
-  play(Uri uri) {
-    _player.setAudioSource(AudioSource.uri(uri));
-    _player.play();
+  JustAudioPlayer() {
+    _player.playerStateStream.listen((state) async {
+      // play next
+      if (_playQueue.isNotEmpty &&
+          state.processingState == ProcessingState.completed) {
+        await Future.delayed(_interval);
+        _playQueueFront();
+      }
+    });
   }
 
   @override
-  stop() {
+  Queue<Uri> getQueue() => _playQueue;
+
+  Future<void> _playQueueFront() async {
+    // すべて再生済み
+    if (_playQueue.isEmpty) {
+      return;
+    }
+    // 再生止める
     _player.stop();
+
+    final uri = _playQueue.removeFirst();
+    print('front: $uri, queue: $_playQueue');
+    await _player.setAudioSource(AudioSource.uri(uri));
+    return _player.play();
+  }
+
+  @override
+  Future<void> playAll(List<Uri> uris) async {
+    _playQueue
+      ..clear()
+      ..addAll(uris);
+    await _playQueueFront();
+  }
+
+  @override
+  Future<void> play(Uri uri) async {
+    _playQueue
+      ..clear()
+      ..add(uri);
+    _playQueueFront();
+  }
+
+  @override
+  void pause() {
+    _player.pause();
   }
 }
 
-class AssetsAudioPlayer implements IPlayer {
+class AssetsPlayer implements IPlayer {
   AssetsAudioPlayer _player = AssetsAudioPlayer();
+  Queue<Uri> _playQueue = Queue();
+  Duration _interval = Duration(milliseconds: 300);
 
-  @override
-  play(Uri uri) {
-    _player.play(uri);
+  AssetsPlayer() {
+    // playAll() 何回か押すと buggy
+    _player.playlistAudioFinished.listen((state) async {
+      if (_playQueue.isNotEmpty) {
+        await Future.delayed(_interval);
+        _playQueueFront();
+      }
+    });
   }
 
   @override
-  stop() {
+  Queue<Uri> getQueue() => _playQueue;
+
+  Future<void> _playQueueFront() async {
+    // すべて再生済み
+    if (_playQueue.isEmpty) {
+      return;
+    }
+    // 再生止める
     _player.stop();
+
+    final uri = _playQueue.removeFirst();
+    // print('front: $uri, queue: $_playQueue');
+    final audio = Audio.file(uri.path);
+    await _player.open(audio);
+    return _player.play();
+  }
+
+  @override
+  Future<void> playAll(List<Uri> uris) async {
+    _playQueue
+      ..clear()
+      ..addAll(uris);
+    _playQueueFront();
+  }
+
+  @override
+  Future<void> play(Uri uri) async {
+    _playQueue
+      ..clear()
+      ..add(uri);
+    await _playQueueFront();
+  }
+
+  @override
+  void pause() {
+    _player.pause();
   }
 }
 
+/// View Model
 class VoiceCachePageNotifier extends ChangeNotifier {
+  static VoiceCachePageNotifier _instance;
+
+  factory VoiceCachePageNotifier() {
+    return _instance ??= VoiceCachePageNotifier._ctor();
+  }
+
+  VoiceCachePageNotifier._ctor() {
+    // _player = AssetsPlayer();
+    _player = JustAudioPlayer();
+  }
+
+  IPlayer _player;
+
+  // IPlayer _player = false ? JustAudioPlayer() : AssetsPlayer();
+
   CacheableFirebaseStorage storage = new CacheableFirebaseStorage();
 
-  // IPlayer _player = AssetsAudioPlayer();
-  IPlayer _player = JustAudioPlayer();
+  Queue<Uri> getQueue() => _player.getQueue();
 
-  void play() async {
-    final uri = await storage.getUri('voices/acting_1_1_en-au.mp3');
-    print('uri: $uri');
-    _player.play(uri);
+  Future<void> playAll() async {
+    final uris = [
+      await storage.getUri('voices/acting_1_1_en-au.mp3'),
+      await storage.getUri('voices/acting_1_1_en-us.mp3'),
+      await storage.getUri('voices/acting_1_2_en-uk.mp3'),
+    ];
+
+    await _player.playAll(uris);
+    print(_player.getQueue());
+    notifyListeners();
+    print('end playAll()');
   }
 
-  void stop() {
-    _player.stop();
+  Future<void> play() async {
+    final uri = await storage.getUri('voices/acting_1_1_en-au.mp3');
+    // print('uri: $uri');
+    await _player.play(uri);
+    notifyListeners();
+    print('end play()');
+  }
+
+  void pause() {
+    _player.pause();
   }
 
   void removeCache() {
-    service.removeCache();
+    storage.removeCache();
   }
 }
 
@@ -90,13 +215,18 @@ class VoiceCachePage extends ConsumerWidget {
       ),
       body: Column(
         children: [
+          Text('${vm.getQueue()}'),
           RaisedButton(
             onPressed: vm.play,
             child: Text('Play'),
           ),
           RaisedButton(
-            onPressed: vm.stop,
-            child: Text('Stop'),
+            onPressed: vm.playAll,
+            child: Text('Play All'),
+          ),
+          RaisedButton(
+            onPressed: vm.pause,
+            child: Text('Pause'),
           ),
           RaisedButton(
             onPressed: vm.removeCache,
